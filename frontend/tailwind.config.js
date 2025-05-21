3c634a6e18e9c59182be00b6f829f6c1ffeecc9b
Shaun-MC <shaunmcushman@gmail.com>
2025-05-21 14:37:52 -0700

    added a new tailwind class color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-
 export const content = [
-  './src/**/*.{html,js,jsx}'
+  './src/**/*.{html,js,jsx}',
 ];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      footerBlue: "#E0E4F4",
+      darkBlue: "#044CAC",
+      darkBlueHover: "#0A4FCE",
+    },
+  },
 };
-
 export const plugins = [];
 
