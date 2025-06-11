46a32d4c3c447d19f9a945f0243302879bf1faa4
Shaun-MC <shaunmcushman@gmail.com>
2025-06-10 17:22:53 -0700

    imp. footer

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p className="footer-text">© 2025 WindowButlers. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
