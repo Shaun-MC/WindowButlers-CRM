b597d01f4cc6c75fd85b72ab8c9f9291b513551c
Shaun-MC <shaunmcushman@gmail.com>
2025-06-10 17:16:31 -0700

    added blank files for unwritten features

---
+++ frontend/src/components/features/appt-scheduling/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppointmentSchedulingPage = () => {
+    return <div style={{ background: "white", width: "100vw", height: "100vh" }} />;
+};
+
+export default AppointmentSchedulingPage;
---
+++ frontend/src/components/features/payment-processing/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentProcessingPage = () => {
+    return <div style={{ background: "white", width: "100vw", height: "100vh" }} />;
+};
+
+export default PaymentProcessingPage;
