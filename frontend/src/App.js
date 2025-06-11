b9d121e26f214d34549402cee80791ce9b4cf541
Shaun-MC <shaunmcushman@gmail.com>
2025-06-10 17:15:30 -0700

    updated App files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,81 @@
-import LoginPage from "./components/login/LoginPage";
-import Header from "./components/header/Header";
-import Home from "./components/home/HomePage";
+import Login from "./components/login/loginpage.jsx";
 
-import { AuthProvider } from "./contexts/authContext";
+import Header from "./components/header/heading.jsx";
+import Footer from "./components/footer/footer.js";
+import HomePage from "./components/homepage/homepage";
+import ManageDataPage from "./components/features/manage-data/index2.jsx";
+import ReferenceMaterialPage from "./components/features/reference-material/index.js";
+import MetricsDashboardPage from "./components/features/metrics-dashboard/index.js";
+import RouteJobsPage from "./components/features/route-jobs/index.js";
+import ClientOutreachPage from "./components/features/client-outreach/index.js";
+import AppointmentSchedulingPage from "./components/features/appt-scheduling/index.js";
+import PaymentProcessingPage from "./components/features/payment-processing/index.js";
+import SettingsPage from "./components/settings/index.js";
+import AccountPage from "./components/account/index.js";
+
+import { AuthProvider } from "./contexts/auth/index.jsx";
 import { useRoutes } from "react-router-dom";
 
-// Weird spacing thing w/ auto formatter in the file
 function App() {
     const routesArray = [
         {
             path: "*",
-            element: <LoginPage />,
+            element: <Login />,
         },
         {
             path: "/login",
-            element: <LoginPage />,
+            element: <Login />,
         },
-
         {
             path: "/home",
-            element: <Home />,
+            element: <HomePage />,
         },
+        {
+            path: "/manage-data",
+            element: <ManageDataPage />,
+        },
+        {
+            path: "/reference-material",
+            element: <ReferenceMaterialPage />,
+        },
+        {
+            path: "/metrics-dashboard",
+            element: <MetricsDashboardPage />,
+        },
+        {
+            path: "/route-jobs",
+            element: <RouteJobsPage />,
+        },
+        {
+            path: "/client-outreach",
+            element: <ClientOutreachPage />,
+        },
+        {
+            path: "/appointment-scheduling",
+            element: <AppointmentSchedulingPage />,
+        },
+        {
+            path: "/payment-processing",
+            element: <PaymentProcessingPage />,
+        },
+        {
+            path: "/settings",
+            element: <SettingsPage />,
+        },
+        {
+            path: "/account",
+            element: <AccountPage />,
+        }
+
     ];
-
     let routesElement = useRoutes(routesArray);
-
     return (
         <AuthProvider>
             <Header />
             <div className="w-full h-screen flex flex-col">{routesElement}</div>
+            <Footer />
         </AuthProvider>
-  );
+    );
 }
 
 export default App;
